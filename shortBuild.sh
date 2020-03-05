4377734478a05a92875cbce28ecfad95a9204209
Pterodactylus <59186787+Pterodactylus@users.noreply.github.com>
2020-03-04 20:08:05 -0600

    Modularize

---
+++ shortBuild.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#sudo bash ./shortBuild.sh
+#cd ~/Ceres.js
+cwd=$(pwd)
+bdir=$(pwd)/build
+
+rm -r $bdir/Ceres.js
+mkdir $bdir/Ceres.js
+cd $bdir/Ceres.js
+
+#This line is returning an error due to problems with the ceres solver. The sed command is a workaround.
+sed -i 's/include(CeresCodeGeneration)/# include(CeresCodeGeneration)/' $bdir/installpkg/lib/cmake/Ceres/CeresConfig.cmake
+$bdir/emsdk/upstream/emscripten/emconfigure cmake $cwd/ -DCMAKE_INSTALL_PREFIX=$bdir/installpkg
+$bdir/emsdk/upstream/emscripten/emmake make
+#cp --verbose $bdir/Ceres.js/Ceres.js $cwd/Ceres-v1.3.5.js
+
+
