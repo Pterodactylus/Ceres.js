074185fc75005a23db1168780dd66c99a8816485
Pterodactylus <59186787+Pterodactylus@users.noreply.github.com>
2020-03-13 22:14:19 -0500

    Update

--- CeresHelper.js
+++ CeresHelper.js
@@ -3,38 +3,37 @@
 //Ceres Helper JS
 
 export class Ceres {
-	constructor(loadedFxn) {
+	constructor() {
 		this.loaded = false
 		this.fxn = []
 		this.lowerbound = []
 		this.upperbound = []
 		this.callback = []
 		
-		CeresModule().then(function(Module){
-			
-			this.instance = new Module.Ceresjs
-			
-			// Create example data to test float_multiply_array
-			this.fxnLength = 0
-			this.length = 0
-			this.maxLength = 1000
-			this.data = new Float64Array(this.maxLength);
+		// Create example data to test float_multiply_array
+		this.varLength = 0
+		this.maxLength = 1000
+		this.data = new Float64Array(this.maxLength);
+		
+		this.promise = new Promise(function(resolve, reject){
+			CeresModule().then(function(Module){
+				this.instance = new Module.Ceresjs
 
-			// Get data byte size, allocate memory on Emscripten heap, and get pointer
-			let nDataBytes = this.data.length * this.data.BYTES_PER_ELEMENT;
-			let dataPtr = Module._malloc(nDataBytes);
+				// Get data byte size, allocate memory on Emscripten heap, and get pointer
+				let nDataBytes = this.data.length * this.data.BYTES_PER_ELEMENT;
+				let dataPtr = Module._malloc(nDataBytes);
 
-			// Copy data to Emscripten heap (directly accessed from Module.HEAPU8)
-			this.dataHeap = new Float64Array(Module.HEAPF64.buffer, dataPtr, nDataBytes);
-			this.dataHeap.set(new Float64Array(this.data.buffer));
-			this.loaded = true
-			loadedFxn();
+				// Copy data to Emscripten heap (directly accessed from Module.HEAPU8)
+				this.dataHeap = new Float64Array(Module.HEAPF64.buffer, dataPtr, nDataBytes);
+				this.dataHeap.set(new Float64Array(this.data.buffer));
+				this.loaded = true
+				resolve()
+			}.bind(this)
 		}.bind(this))
 	}
 	// Method
 	add_function(fn) {
 		this.fxn.push(fn)
-		this.fxnLength = this.fxnLength + 1;
 	}
 	// Method
 	add_lowerbound(xNumber, lowerBound) {
@@ -60,7 +59,7 @@ export class Ceres {
 	load_fxns(){
 		for(let i = 0; i < this.fxn.length; i++){
 			let newfunc = function f(){
-				let x = new Float64Array(this.dataHeap.buffer, this.dataHeap.byteOffset, this.length);
+				let x = new Float64Array(this.dataHeap.buffer, this.dataHeap.byteOffset, this.varLength);
 				return this.fxn[i](x)
 			}
 			this.instance.add_function(newfunc.bind(this));
@@ -73,7 +72,7 @@ export class Ceres {
 		}
 		for(let i = 0; i < this.callback.length; i++){
 			let newfunc = function f(evaluate_jacobians, new_evaluation_point){
-				let x = new Float64Array(this.dataHeap.buffer, this.dataHeap.byteOffset, this.length);
+				let x = new Float64Array(this.dataHeap.buffer, this.dataHeap.byteOffset, this.varLength);
 				return this.callback[i](x, evaluate_jacobians, new_evaluation_point);
 			}
 			this.instance.add_callback(newfunc.bind(this));
@@ -82,7 +81,7 @@ export class Ceres {
 	// Method
 	solve(xi, max_numb_iterations = 2000, parameter_tolerance = 1e-10, function_tolerance = 1e-16, gradient_tolerance = 1e-16) {
 		if(this.loaded == true){
-			if(this.length <= this.maxLength ){this.length = this.fxnLength}
+			if(this.varLength <= this.maxLength ){this.varLength = this.fxn.length}
 			else{throw "Max number of vars exceeded"}
 			
 			this.load_fxns()
@@ -90,7 +89,7 @@ export class Ceres {
 			for(let i = 0; i < xi.length; i++){
 				this.dataHeap[i] = xi[i];
 			}
-			this.instance.setup_x(this.dataHeap.byteOffset, this.length);
+			this.instance.setup_x(this.dataHeap.byteOffset, this.varLength);
 			let max_num_iterations = max_numb_iterations
 			let parameter_tol = parameter_tolerance
 			let function_tol = function_tolerance
@@ -99,7 +98,7 @@ export class Ceres {
 			let report = this.instance.get_report();
 			let message = this.instance.get_message();
 			//console.log(report)
-			let x = new Float64Array(this.dataHeap.buffer, this.dataHeap.byteOffset, this.length)
+			let x = new Float64Array(this.dataHeap.buffer, this.dataHeap.byteOffset, this.varLength)
 			let normalArray = [].slice.call(x);
 			let txt = "";
 			for(let i=0; i<normalArray.length; i++){
