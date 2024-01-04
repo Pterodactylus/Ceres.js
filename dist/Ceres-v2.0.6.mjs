7affbd5ce9f8b77aad07b6473e328b628d4e8f53
miner <pterosauria@protonmail.com>
2024-01-03 19:22:34 -0500

    renamed:    dist/Ceres-v2.0.5.mjs -> dist/Ceres-v2.0.6.mjs

--- dist/Ceres-v2.0.6.mjs
+++ dist/Ceres-v2.0.6.mjs
@@ -4619,6 +4619,7 @@ export class Ceres {
 	async run(jsonSystem, max_numb_iterations = 2000, parameter_tolerance = 1e-10, function_tolerance = 1e-16, gradient_tolerance = 1e-16, max_solver_time_in_seconds = 100, initial_trust_region_radius = 1e4, max_trust_region_radius = 1e16, max_num_consecutive_invalid_steps = 5) {
       this.setSystemFromJson(jsonSystem);
       let initial_guess = this.generateInitialGuess(jsonSystem.variables);
-		  return results = await this.solve(initial_guess, max_numb_iterations, parameter_tolerance, function_tolerance, gradient_tolerance, max_solver_time_in_seconds, initial_trust_region_radius, max_trust_region_radius, max_num_consecutive_invalid_steps);
+		  const results = await this.solve(initial_guess, max_numb_iterations, parameter_tolerance, function_tolerance, gradient_tolerance, max_solver_time_in_seconds, initial_trust_region_radius, max_trust_region_radius, max_num_consecutive_invalid_steps);
+      return results
 	}
 }
