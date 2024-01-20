ce99b2ea2d9f9d627cfedd2c86534f764a563149
Pterodactylus <you@example.com>
2024-01-20 18:05:45 -0500

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -70,6 +70,7 @@ cd $bdir/Ceres.js
 #$bdir/emsdk/upstream/emscripten/emconfigure cmake $cwd/ -DCMAKE_INSTALL_PREFIX=$bdir/installpkg
 $bdir/emsdk/upstream/emscripten/emcmake cmake $cwd/ -DCMAKE_INSTALL_PREFIX=$bdir/installpkg
 $bdir/emsdk/upstream/emscripten/emmake make
+cat $cwd/evaluatex.min.js >> $bdir/Ceres.js/Ceres.js
 cat $cwd/CeresHelper.js >> $bdir/Ceres.js/Ceres.js
 cp --verbose $bdir/Ceres.js/Ceres.js $cwd/dist/ceres.js
 
